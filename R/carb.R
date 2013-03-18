b29636bb28b93503b0bc1128957f2a84bafc888a
Jean-Pierre Gattuso <gattuso2@obs-vlfr.fr>
2013-03-18 15:52:22 +0100

    Change made on carb.R by JME

--- R/carb.R
+++ R/carb.R
@@ -9,7 +9,7 @@
 # Seacarb is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with seacarb; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-# JME optimized
+# JME optimized.
 #
 #
 carb<-
